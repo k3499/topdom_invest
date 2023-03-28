02aa8b6b61fc9daebef01e42e71286af6f4e70ea
k3499 <k3499@yandex.ru>
2023-03-28 13:34:52 +0300

    form validation and policy page

--- js/script.js
+++ js/script.js
@@ -86,15 +86,24 @@ function lightroom(rustv){
 
 document.addEventListener('DOMContentLoaded', () => {
     let submitButton = document.querySelector('.text-form__submit');
+    document.querySelector('.text-form')
+    .addEventListener('input', function(ev) {
+    document.querySelector('.text-form__submit')
+    .disabled = !([...this.querySelectorAll('input[type=text]:valid')].filter(el => el.value).length == 2) || !this.querySelector('input[type=checkbox]:checked');
+        //Валидация полей и выключение кнопки
+});
+
     submitButton.addEventListener('click', (evt) =>{
         evt.preventDefault();
-        let name = document.querySelector('.name');
+        
         let tel = document.querySelector('.tel');
+        let check = document.querySelector('.text-form__checkbox');
         let result = document.querySelector('.form__subtitle');
         let form = document.querySelector('.text-form');
         const url ="https://dev.salesevolution.ru/pub/source-connector/form-getter.php/topdomkzn/secondsite"
         const formData = new FormData(form);
         const payload = JSON.stringify(Object.fromEntries(formData));
+        //console.log(check.checked)
         console.log(payload)
         fetch(url, {
             method: 'POST',
@@ -108,25 +117,26 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(data)
             ym(92719188,'reachGoal','lead_from_form')
           })
-        //запрос пришел
-        // xhr.onreadystatechange = function () {
-        // // если запрос принят и сервер ответил, что всё в порядке
-        // if (xhr.readyState === 4 && xhr.status === 200) {
-        //     // выводим то, что ответил нам сервер — так мы убедимся, что данные он получил правильно
-        //     result.innerHTML = this.responseText;
-        // }
-        // // преобразуем наши данные JSON в строку
-        // var data = JSON.stringify({ "name": name.value, "tel": tel.value });
-        // // когда всё готово, отправляем JSON на сервер
-        // xhr.send(data);
-        // };
         console.log(evt)
     }
 )
 })
 
+// function formValidation(payload){
+//     if(payload.name && payload.tel && check.checked){
+//         console.log("все кул")
+//     }else if(!payload.name){
+//         console.log("имя не кул")
+//     }else if(!payload.tel){
+//         console.log("tel не кул")
+//     }
 
-document.addEventListener('DOMContentLoaded', () => {
+// }
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
     
 
     //const consultBlock = document.getElementById('consultation-thanks')
@@ -150,56 +160,56 @@ document.addEventListener('DOMContentLoaded', () => {
     // })
     
     
-    forms.forEach((form) => {
-      form.addEventListener('submit', submit)
-    })
+    // forms.forEach((form) => {
+    //   form.addEventListener('submit', submit)
+    // })
     
-    function submit(e) {
-      e.preventDefault()
-      const form = e.target
+    // function submit(e) {
+    //   e.preventDefault()
+    //   const form = e.target
     
-      const names = form.querySelectorAll('.name');
-      const regex = /[<>&]/
-      let isValidData
-      let isName = false
-      let isPhone = false
+    //   const names = form.querySelectorAll('.name');
+    //   const regex = /[<>&]/
+    //   let isValidData
+    //   let isName = false
+    //   let isPhone = false
     
-      names.forEach(name => {
-        if (name.value && !regex.test(name.value)) {
-          isName = true
-          name.removeAttribute('style')
-        } else {
-          name.style.borderColor = 'red'
-        }
-      })
-      phones.forEach(phone => {
-        if (phone.value && phone.value.length >= 7) {
-          isPhone = true
-          phone.removeAttribute('style')
-        } else {
-          phone.style.borderColor = 'red'
-        }
-      })
+    //   names.forEach(name => {
+    //     if (name.value && !regex.test(name.value)) {
+    //       isName = true
+    //       name.removeAttribute('style')
+    //     } else {
+    //       name.style.borderColor = 'red'
+    //     }
+    //   })
+    //   phones.forEach(phone => {
+    //     if (phone.value && phone.value.length >= 7) {
+    //       isPhone = true
+    //       phone.removeAttribute('style')
+    //     } else {
+    //       phone.style.borderColor = 'red'
+    //     }
+    //   })
     
-      isValidData = isName * isPhone
-      console.log(isValidData)
+    //   isValidData = isName * isPhone
+    //   console.log(isValidData)
     
-      if (isValidData) {
-        const payload = new FormData(form);
-        // console.log([...payload]);
-        fetch('/blagodarim', {
-          method: 'POST',
-          body: payload
-        })
-        // .then(res => res.json())
-        .then((data) => {
-          console.log(data)
-          ym(92719188,'reachGoal','lead_from_form')
-        })
-        // .then(data => console.log(data))
+    //   if (isValidData) {
+    //     const payload = new FormData(form);
+    //     // console.log([...payload]);
+    //     fetch('/blagodarim', {
+    //       method: 'POST',
+    //       body: payload
+    //     })
+    //     // .then(res => res.json())
+    //     .then((data) => {
+    //       console.log(data)
+    //       ym(92719188,'reachGoal','lead_from_form')
+    //     })
+    //     // .then(data => console.log(data))
     
-        form.style.display = 'none'
-        form.nextElementSibling.style.display = 'block'
-      }
-    }
-    })
+    //     form.style.display = 'none'
+    //     form.nextElementSibling.style.display = 'block'
+    //   }
+    // }
+    // })
